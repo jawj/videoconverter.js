4a24183897ddd99a60cf40d633e9669381d2da79
George MacKerron <george@mackerron.co.uk>
2014-04-22 16:33:17 +0100

    Tweaks and .gitignore

--- build/build_custom_gm.sh
+++ build/build_custom_gm.sh
@@ -10,13 +10,13 @@ cd ffmpeg
 emconfigure ./configure --cc="emcc" --enable-cross-compile --target-os=none --arch=x86_32 --cpu=generic --disable-ffplay --disable-ffprobe --disable-ffserver --disable-asm --disable-doc --disable-devices --disable-pthreads --disable-w32threads --disable-network --disable-hwaccels --disable-parsers --disable-bsfs --disable-debug --disable-zlib --disable-protocols --disable-indevs --disable-outdevs --enable-protocol=file --enable-pic --enable-small \
   --disable-demuxers --enable-demuxer=image2,wav \
   --disable-decoders --enable-decoder=pam,pcm_s16le \
-  --disable-encoders --enable-encoder=mpeg1video,h263,mp2,mp3,mpeg4
+  --disable-encoders --enable-encoder=mpeg1video,h263,mp2,aac,mpeg4
 
 make clean
 make
 cp ffmpeg ffmpeg.bc
 
-emcc -s OUTLINING_LIMIT=100000 -s TOTAL_MEMORY=33554432 -Os --closure 1 -v ffmpeg.bc -o ../ffmpeg_asm.js --pre-js ../ffmpeg_pre.js --post-js ../ffmpeg_post.js
+emcc -s OUTLINING_LIMIT=100000 -s TOTAL_MEMORY=33554432 -Os --closure 1 -v ffmpeg.bc -o ../ffmpeg-custom.js --pre-js ../ffmpeg_pre.js --post-js ../ffmpeg_post.js
 
 cd ../
 
